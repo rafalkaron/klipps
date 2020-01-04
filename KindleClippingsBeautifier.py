ba05b9e5413f4eb57493c375e6aaa204c8331b7c
rafalkaron <rafalkaron@gmail.com>
2020-01-04 12:34:07 +0100

    Output file enumeration works

--- KindleClippingsBeautifier.py
+++ KindleClippingsBeautifier.py
@@ -1,9 +1,9 @@
 # coding: utf-8
-__version__ = "0.2"
+__version__ = "0.3"
 __author__ = "Rafał Karoń <rafalkaron@gmail.com.com>"
 
 """
-    Kindle Clippings Beautifier (Codename: Unchanging Tick)
+    Kindle Clippings Beautifier (Codename: Sequential Tick)
     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
     Beautify your Kindle clippings by exporting them to a desired format.
@@ -17,14 +17,14 @@ __author__ = "Rafał Karoń <rafalkaron@gmail.com.com>"
     - CLI
     - GUI
     - EXE
-    - Enumerate output files
     - Default settings
     - Customization (Name, e.g. Fred's Clippings)
 """
 #Modules
 import os
 import datetime
-import re       #regular expression
+import re           #regular expression
+
 #Global Variables
 _timestamp = datetime.datetime.now()
 _timestamp_formatted = str(_timestamp.strftime("%d_%m_%y-%H-%M-%S"))
@@ -51,10 +51,9 @@ def main_menu():
 def kindle_to_md():
     global _kindle_to_md_called
     _kindle_to_md_called = True
-    
     out_folder()
-    global _out_md
-    _out_md = open(str(_out_folder) + "/" + "My Clippings" + ".md", "w+t", encoding="utf-8")
+    out_file()
+
     _clippings_filepath = input("Enter a full path to the \"Kindle Clippings.txt\" file: ")
     _clippings_file = open(_clippings_filepath, "rt", encoding ="utf-8")
 
@@ -63,7 +62,7 @@ def kindle_to_md():
     with _clippings_file as file:
         filedata = file.read()
     filedata = filedata.replace("==========", "") #.replace can be stacked
-    with _out_md as file:
+    with _out_md as file: ##remove open
         file.write(filedata)
 
     file.close
@@ -74,7 +73,7 @@ def kindle_to_pdf():
     global _kindle_to_pdf_called
     _kindle_to_pdf_called = True
     kindle_to_md()
-    #os.system("cd "+ str(_script_directory) + "DITA-OT/bin && dita -i " + "\"../../out/My Clippings.md\"" + " -f pdf2")     #improve path handling
+    os.system("cd "+ str(_script_directory) + "DITA-OT/bin && dita -i " + "\"../../out/My Clippings.md\"" + " -f pdf2")     #improve path handling
 
 _kindle_to_pdf_called = False
 
@@ -86,20 +85,28 @@ _kindle_to_html5_called = False
 def output_title_header():
     _out_md.write("# My Kindle Clippings" + "\n" + " _Generated on " + str(_timestamp.strftime("%x")) + " at " + str(_timestamp.strftime("%X")) + "_\n\n")
 
+def out_file():
+    i = 0
+    if _kindle_to_md_called == True:
+        while os.path.exists(str(_out_folder) + "/" + "My Clippings %s" %i + ".md"):
+            i += 1
+        global _out_md
+        _out_md = open(str(_out_folder) + "/" + "My Clippings %s" %i + ".md", "w+t", encoding="utf-8")
+
 def out_folder():
     global _out_folder
-    _out_folder = ("out")
+    _out_folder = (str(_script_directory) + "out")
     if not os.path.exists(_out_folder):
         os.mkdir(_out_folder)
 
 def src_folder():
     global _src_folder
-    _src_folder = ("src")
+    _src_folder = (str(_script_directory) + "src")
     if not os.path.exists(_src_folder):
         os.mkdir(_src_folder)
 
 def summary():
-    if _kindle_to_md_called == True and _kindle_to_pdf_called == False:             #improve this by adding HTML5 option
+    if _kindle_to_md_called == True and _kindle_to_pdf_called == False and _kindle_to_html5_called == False:
         print("Your Kindle clippings were converted to Markdown.")
     elif _kindle_to_pdf_called == True:
         print("Your Kindle clippings were converted to PDF.")
@@ -109,7 +116,7 @@ def summary():
 #Invocations
 intro()
 #main_menu()
-#kindle_to_md()
-kindle_to_pdf()
+kindle_to_md()
+#kindle_to_pdf()
 summary()
 exit()
---
+++ sandbox.py
@@ -0,0 +1,3 @@
+i = 1
+
+print("test%s" %i)
