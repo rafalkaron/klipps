0d5778a6b0b6e34975e303af6a0d2626adccea9a
rafalkaron <rafalkaron@gmail.com>
2020-04-17 18:22:37 +0200

    update

--- KindleClippingsBeautifier.py
+++ KindleClippingsBeautifier.py
@@ -1,135 +1,28 @@
 # coding: utf-8
-
-__version__ = "0.3.1"
-__author__ = "Rafał Karoń <rafalkaron@gmail.com.com>"
-
 """
 Export your Kindle Clippings to a nice PDF.
 """
 
-import os, datetime, re, shutil
+__version__ = "0.1"
+__author__ = "Rafał Karoń <rafalkaron@gmail.com.com>"
 
-_timestamp = datetime.datetime.now()
-_timestamp_formatted = str(_timestamp.strftime("%d_%m_%y-%H-%M-%S"))
-_script_filepath = os.path.abspath(__file__)                                            #gets this script filepath
-_script_filename = os.path.basename(__file__)                                           #gets this script filename
-_script_directory = _script_filepath.replace(_script_filename, "").replace("\\", "/")   #prints the script directory w/o its filename
+from dirs import enter_filepath
+from read import read_file_lines
 
-#Functions
-def intro():
-    """Displays hello message."""
-    print("Beautify your Kindle clippings by exporting them to a Markdown file.")
+filepath = enter_filepath()
+file_lines = read_file_lines(filepath)
 
-def main_menu():
-    _output_type = input("Select the output format for your Kindle clippings by entering one of the following:\n[md] to convert your Kindle clippings to Markdown\n[pdf] to convert your Kindle clippings to PDF\n[html] to convert your Kindle clippings to a website format (HTML5)\n")
-    if _output_type == "md" or _output_type == "MD" or _output_type == "[md]" or _output_type == "[MD]":
-        kindle_to_md()
-    elif _output_type == "pdf" or _output_type == "PDF" or _output_type == "[pdf]" or _output_type == "[PDF]":
-        kindle_to_pdf()
-    elif _output_type == "html" or _output_type == "HTML" or _output_type == "[html]" or _output_type == "[HTML]":
-        kindle_to_html5()
-    else:
-        print("Try answering the following question again.")
-        main_menu()
+def convert_to_markdown(file_lines):
+    """Marks up a list of Kindle Clipping lines with Markdown Syntax"""
+    print(file_lines)
+    #return markdown
 
-def feed():
-    global _clippings_filepath
-    _clippings_filepath = input("Enter a full path to the \"Kindle Clippings.txt\" file: ")
-
-def kindle_to_md():
-    #Checks if the function was called
-    global _kindle_to_md_called
-    _kindle_to_md_called = True
-    #Folders and Files
-    feed()
-    tmp()
-    out()
-    #Document Manipulation
-    _header = str("# My Kindle Clippings" + "\n" + " _Generated on " + str(_timestamp.strftime("%x")) + " at " + str(_timestamp.strftime("%X")) + "_\n\n")    
-    
-    with open(_clippings_filepath, "rt", encoding ="utf-8") as _in_file:
-        _clippings_filepath_lines = _in_file.readlines()
-        for line in _clippings_filepath_lines:
-            if line.startswith("=========="):
-                line = line + "## "
-            _tmp_md.write(line)
-
-    with open(_tmp_md_filepath, "rt", encoding ="utf-8") as _in_file:
-        _sr = _in_file.read()
-        _sr = re.sub("==========", "", _sr)                                         #Removes underlines //h2in a new line? #remove clippings file @begginign? Inswert newline below. Alternative - check for - Your gihglight and place ## two lines below
-        _sr = re.sub(r"- Your Highlight at location.* \| ", "", _sr)                #Removes redundant highlight location
-        _sr = re.sub('\n\n','\n', _sr)                                              #Removes empty lines
-
-    with open (_out_md_filepath, "w+t", encoding="utf-8") as _out_file:
-        _out_file.write(_header)
-        _out_file.write(_sr)
-_kindle_to_md_called = False
-
-def kindle_to_pdf():
-    global _kindle_to_pdf_called
-    _kindle_to_pdf_called = True
-    kindle_to_md()
-    os.system("cd "+ str(_script_directory) + "DITA-OT/bin && dita -i " + "\"../../out/My Clippings.md\"" + " -f pdf2")     #improve path handling #think about prince
-_kindle_to_pdf_called = False
-
-def kindle_to_html5():
-    global _kindle_to_html5_called
-    _kindle_to_html5_called = True
-_kindle_to_html5_called = False
-
-def tmp(): #consider using the tempfile module
-    global _tmp_folder
-    _tmp_folder = (str(_script_directory) + "tmp")
-    if not os.path.exists(_tmp_folder):
-        os.mkdir(_tmp_folder)    
-    global _tmp_md
-    _tmp_md = open(str(_tmp_folder) + "/tmp.md", "w+t", encoding="utf-8")
-    global _tmp_md_filepath
-    _tmp_md_filepath = str((_tmp_folder) + "/tmp.md")
-
-def cleanup():
-    _tmp_md.close()
-    if os.path.exists(_tmp_folder):
-        shutil.rmtree(_tmp_folder)
-    #if _out_md_filepath == str((_out_folder) + "/My Clippings (0)"):
-    #    os.rename(r"str((_out_folder) + "/My Clippings (0)")", r"str((_out_folder) + "/My Clippings""))
-
-def out():
-    global _out_folder
-    _out_folder = (str(_script_directory) + "out")
-    if not os.path.exists(_out_folder):
-        os.mkdir(_out_folder)
-    i = 0
-    if _kindle_to_md_called == True:
-        while os.path.exists(str(_out_folder) + "/My Clippings (%s).md" %i):
-            i += 1
-        global _out_md
-        _out_md = open(str(_out_folder) + "/My Clippings (%s).md" %i, "w+t", encoding="utf-8")
-        global _out_md_filepath
-        _out_md_filepath = str((_out_folder) + "/My Clippings (%s).md" %i)
-
-def src_folder():
-    global _src_folder
-    _src_folder = (str(_script_directory) + "src")
-    if not os.path.exists(_src_folder):
-        os.mkdir(_src_folder)
-
-def summary():
-    if _kindle_to_md_called == True and _kindle_to_pdf_called == False and _kindle_to_html5_called == False:
-        print("Your Kindle clippings were converted to Markdown.")
-    elif _kindle_to_pdf_called == True:
-        print("Your Kindle clippings were converted to PDF.")
-    elif _kindle_to_html5_called == True:
-        print("Your Kindle clippings were converted to a website format (HTML5).")
+def save(markdown_file):
+    """Saves a Markdown file"""
 
 
 def main():
-    intro()
-    #main_menu()
-    kindle_to_md()
-    cleanup()
-    summary()
-    exit()
+    convert_to_markdown(file_lines)
 
 if __name__ == "__main__":
     main()
--- read.py
+++ read.py
@@ -1,4 +1,9 @@
-def read_file(file):
+# coding: utf-8
+"""
+
+"""
+
+def read_file_lines(file):
     """Returns a list of str lines in text file"""
     with open(file, mode="rt", encoding="utf-8") as in_file:
         file_lines = in_file.readlines()
