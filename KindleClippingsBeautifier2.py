53e6bef62dc964aab6a792e4ed5c23e8f9566521
rafalkaron <rafalkaron@gmail.com>
2020-04-17 15:08:20 +0200

    update

--- KindleClippingsBeautifier2.py
+++ KindleClippingsBeautifier2.py
@@ -11,13 +11,19 @@ from dirs import enter_filepath
 filepath = enter_filepath()
 
 def read(file):
-    """Generates a list of str lines in the file"""
+    """Returns a list of str lines in text file"""
     with open(file, mode="rt", encoding="utf-8") as in_file:
-        f = in_file.readlines()
-        print(f)
+        global file_lines
+        file_lines = in_file.readlines()
+    return file_lines
+
+def convert_to_markdown(file_lines):
+    """Mark up a list of Kindle Clipping lines with Markdown Syntax"""
+    print(file_lines)
 
 def main():
     read(filepath)
+    convert_to_markdown(file_lines)
 
 if __name__ == "__main__":
     main()
