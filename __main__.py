fa3a4a104c1aa93acb5a8b87bb2dbca66e29188f
rafalkaron <rafalkaron@gmail.com>
2020-04-30 14:33:43 +0200

    working on automation

--- __main__.py
+++ __main__.py
@@ -14,33 +14,43 @@ from Klipps import (read_file,
                     open_file,
                     progressbar as pb)
 
-__version__ = "0.6"
+__version__ = "0.7"
 __author__ = "Rafał Karoń <rafalkaron@gmail.com.com>"
 
 def main():
     par = argparse.ArgumentParser(description="Export Kindle clippings to HTML.", formatter_class=argparse.RawTextHelpFormatter)
-    #par.add_argument("kindle_clippings_path", help="The file path to the \"My Clippings.txt\" file that you want to convert to HTML.\nFor example: \"D:\Kindle\Documents\My Clippings.txt\" (Windows) or \"/Volumes/Kindle/documents/My Clippings.txt\" (macOS).\nNote: Remember to encapsulate the path in inverted commas!")
+    par.add_argument("-in", "--input", help="Specify the \"My Clippings.txt\" file path manually.")
+    par.add_argument("-out", "--output", help="Specify the HTML file path converted from the \"My Clippings.txt\" file manually.")
     par.add_argument("-nopr", "--no_preview", action="store_true", help="does not open the html file converted from the \"My Clippings.txt\" file")
     par.add_argument("-ns", "--no_style", action ="store_true", help="does not add CSS styling to the html file converted from the \"My Clippings.txt\" file")
     par.add_argument("-v", "--version", action="version", version=f"%(prog)s {__version__}")
-    #if len(sys.argv)==1:
-    #    par.print_help(sys.stderr)
-    #    sys.exit(1)
     args = par.parse_args()
     
     if os.name == "nt":
         try:
-            kindle_clippings_path = r"D:\documents\My Clippings.txt"
+            clipps_path = r"E:\documents\My Clippings.txt"
+            clipps_exist = os.path.isfile(clipps_path)
         except:
-            kindle_clippings_path = r"E:\documents\My Clippings.txt"
+            clipps_path = r"D:\documents\My Clippings.txt"
+            clipps_exist = os.path.isfile(clipps_path)
+        else:
+            print("Klipps cannot locate your Kindle Clippings file!' The \"My Clippings.txt\" file is saved in the \"Documents\" directory on your Kindle device.\nNote: Ensure that your Kindle is connected and discovered by the computer. Some third-party USB cables may prevent your computer from correctly discovering your Kindle device.")
+        finally:
+            if clipps_exist == False:
+                clipps_path = input("\nProvide the file path to the \"My Clippings.txt\" file manually or press [Enter] to exit Klipps: ")
+                filepath_exist = os.path.isfile(clipps_path)
+                if filepath_exist == False:
+                    print("")
+                    sys.exit(0)
 
     if os.name == "posix":
-        kindle_clippings_path = r"/Volumes/Kindle/documents/My Clippings.txt"
+        clipps_path = r"/Volumes/Kindle/documents/My Clippings.txt"
+        clipps_exist = os.path.isfile(clipps_path)
 
     pb(10)
-    html_str = clipps_str_to_html_str(read_file(kindle_clippings_path))
+    html_str = clipps_str_to_html_str(read_file(clipps_path))
     pb(25)
-    html_path = kindle_clippings_path.replace(".txt", ".html")
+    html_path = clipps_path.replace(".txt", ".html")
     pb(50)    
     if not args.no_style:
         html_str = style_html_str(html_str)
