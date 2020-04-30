2d753b5e865fc7313732e202a5bdabf5d9b23763
rafalkaron <rafalkaron@gmail.com>
2020-04-30 18:41:56 +0200

    to win

--- Klipps/feed.py
+++ Klipps/feed.py
@@ -8,25 +8,33 @@ def clipps_filepath():
     """Return the \"My Clippings.txt\" file path"""
 
     if os.name == "nt":
-        clipps_path = r"D:\documents\My Clippings.txt"
-        clipps_exist = os.path.isfile(clipps_path)
-        if clipps_exist == False:
-            print("Klipps cannot locate your Kindle Clippings file!' The \"My Clippings.txt\" file is saved in the \"Documents\" directory on your Kindle device.\nNote: Ensure that your Kindle is connected and discovered by the computer. Some third-party USB cables may prevent your computer from correctly discovering your Kindle device.")
-            clipps_path = input("\nProvide the file path to the \"My Clippings.txt\" file manually or press [Enter] to exit Klipps: ")
-            filepath_exist = os.path.isfile(clipps_path)
-            if filepath_exist == False:
-                print("")
-                sys.exit(0)
+        volumes = ["D", "E", "F", "G", "H"]
+        for volume in volumes:
+            try:
+                clipps_path = volume + r":\documents\My Clippings.txt"
+                clipps_exist = os.path.isfile(clipps_path)
+                break
+            except:
+                continue
+        
+        return clipps_path
+
 
     if os.name == "posix":
         clipps_path = r"/Volumes/Kindle/documents/My Clippings.txt"
         clipps_exist = os.path.isfile(clipps_path)
+        if clipps_exist == False:
+            enter_clipps_filepath()
+        return clipps_path
 
+def enter_clipps_filepath():
+    print("Klipps cannot locate your Kindle Clippings file!' The \"My Clippings.txt\" file is saved in the \"Documents\" directory on your Kindle device.\nNote: Ensure that your Kindle is connected and discovered by the computer. Some third-party USB cables may prevent your computer from correctly discovering your Kindle device.")
+    clipps_path = input("\nProvide the file path to the \"My Clippings.txt\" file manually or press [Enter] to exit Klipps: ")
+    filepath_exist = os.path.isfile(clipps_path)
+    if filepath_exist == False:
+        print("")
+        sys.exit(0)    
     return clipps_path
-
-def enter_filepath(prompt):
-    filepath = input("\nProvide the file path to the \"My Clippings.txt\" file manually or press [Enter] to exit Klipps: ")
-    return filepath
 
 def read_file(filepath):
     """Return a string with file contents."""
