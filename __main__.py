1c8c0573d89c99360488461057a772735650a86c
rafalkaron <rafalkaron@gmail.com>
2020-04-30 13:48:19 +0200

    multiline help

--- __main__.py
+++ __main__.py
@@ -18,15 +18,16 @@ __version__ = "0.6"
 __author__ = "Rafał Karoń <rafalkaron@gmail.com.com>"
 
 def main():
-    par = argparse.ArgumentParser(description="Export Kindle clippings to HTML.")
-    par.add_argument("kindle_clippings_path", help=r"The file path to the \"My Clippings.txt\" file that you want to convert to HTML.\nFor example: \"D:\Kindle\Documents\My Clippings.txt\" (Windows) or \"/Volumes/Kindle/documents/My Clippings.txt\" (macOS). Remember to encapsulate the path in inverted commas!")
-    par.add_argument("-nopr", "--no_preview", action="store_true", help="Prevents the converted files from opening.")
-    par.add_argument("-ns", "--no_style", action ="store_true", help="Does not style the HTML file by embedding CSS syntax.")
+    par = argparse.ArgumentParser(description="Export Kindle clippings to HTML.", formatter_class=argparse.RawTextHelpFormatter)
+    par.add_argument("kindle_clippings_path", help="The file path to the \"My Clippings.txt\" file that you want to convert to HTML.\nFor example: \"D:\Kindle\Documents\My Clippings.txt\" (Windows) or \"/Volumes/Kindle/documents/My Clippings.txt\" (macOS).\nNote: Remember to encapsulate the path in inverted commas!")
+    par.add_argument("-nopr", "--no_preview", action="store_true", help="does not open the html file converted from the \"My Clippings.txt\" file")
+    par.add_argument("-ns", "--no_style", action ="store_true", help="does not add CSS styling to the html file converted from the \"My Clippings.txt\" file")
     par.add_argument("-v", "--version", action="version", version=f"%(prog)s {__version__}")
     if len(sys.argv)==1:
         par.print_help(sys.stderr)
         sys.exit(1)
     args = par.parse_args()
+    
     pb(10)
     html_str = clipps_str_to_html_str(read_file(args.kindle_clippings_path))
     pb(25)
