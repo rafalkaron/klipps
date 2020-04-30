f61484c3e0ce1c15c1259f5d64186866a646c882
rafalkaron <rafalkaron@gmail.com>
2020-04-30 13:54:55 +0200

    automatic conversion

--- __main__.py
+++ __main__.py
@@ -19,19 +19,28 @@ __author__ = "Rafał Karoń <rafalkaron@gmail.com.com>"
 
 def main():
     par = argparse.ArgumentParser(description="Export Kindle clippings to HTML.", formatter_class=argparse.RawTextHelpFormatter)
-    par.add_argument("kindle_clippings_path", help="The file path to the \"My Clippings.txt\" file that you want to convert to HTML.\nFor example: \"D:\Kindle\Documents\My Clippings.txt\" (Windows) or \"/Volumes/Kindle/documents/My Clippings.txt\" (macOS).\nNote: Remember to encapsulate the path in inverted commas!")
+    #par.add_argument("kindle_clippings_path", help="The file path to the \"My Clippings.txt\" file that you want to convert to HTML.\nFor example: \"D:\Kindle\Documents\My Clippings.txt\" (Windows) or \"/Volumes/Kindle/documents/My Clippings.txt\" (macOS).\nNote: Remember to encapsulate the path in inverted commas!")
     par.add_argument("-nopr", "--no_preview", action="store_true", help="does not open the html file converted from the \"My Clippings.txt\" file")
     par.add_argument("-ns", "--no_style", action ="store_true", help="does not add CSS styling to the html file converted from the \"My Clippings.txt\" file")
     par.add_argument("-v", "--version", action="version", version=f"%(prog)s {__version__}")
-    if len(sys.argv)==1:
-        par.print_help(sys.stderr)
-        sys.exit(1)
+    #if len(sys.argv)==1:
+    #    par.print_help(sys.stderr)
+    #    sys.exit(1)
     args = par.parse_args()
     
+    if os.name == "nt":
+        try:
+            kindle_clippings_path = r"D:\documents\My Clippings.txt"
+        except:
+            kindle_clippings_path = r"E:\documents\My Clippings.txt"
+
+    if os.name == "posix":
+        kindle_clippings_path = r"/Volumes/Kindle/documents/My Clippings.txt"
+
     pb(10)
-    html_str = clipps_str_to_html_str(read_file(args.kindle_clippings_path))
+    html_str = clipps_str_to_html_str(read_file(kindle_clippings_path))
     pb(25)
-    html_path = args.kindle_clippings_path.replace(".txt", ".html")
+    html_path = kindle_clippings_path.replace(".txt", ".html")
     pb(50)    
     if not args.no_style:
         html_str = style_html_str(html_str)
