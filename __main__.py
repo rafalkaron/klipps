35f871f3ec8c8df54d66ae0d99b31b4eb46a49a9
rafalkaron <rafalkaron@gmail.com>
2020-04-30 13:35:52 +0200

    added version info

--- __main__.py
+++ __main__.py
@@ -18,18 +18,19 @@ __version__ = "0.6"
 __author__ = "Rafał Karoń <rafalkaron@gmail.com.com>"
 
 def main():
-    par = argparse.ArgumentParser(description="Export Kindle clippings to HTML.")
-    par.add_argument('clipps_path', help=r"A file path to the \"My Clippings.txt\" file on your Kindle device that you want to convert to HTML. For example: \"D:\Kindle\Documents\My Clippings.txt\" (Windows) or \"/Volumes/Kindle/documents/My Clippings.txt\" (macOS). Remember to encapsulate the path in inverted commas!")
+    par = argparse.ArgumentParser(prog="Klipps", description="Export Kindle clippings to HTML.")
+    par.add_argument("kindle_clippings_path", help=r"The file path to the \"My Clippings.txt\" file that you want to convert to HTML.\nFor example: \"D:\Kindle\Documents\My Clippings.txt\" (Windows) or \"/Volumes/Kindle/documents/My Clippings.txt\" (macOS). Remember to encapsulate the path in inverted commas!")
     par.add_argument("-nopr", "--no_preview", action="store_true", help="Prevents the converted files from opening.")
-    par.add_argument("-ns", "--no_style", action = "store_true",help="Does not style the HTML file by embedding CSS syntax.")
+    par.add_argument("-ns", "--no_style", action ="store_true", help="Does not style the HTML file by embedding CSS syntax.")
+    par.add_argument("-v", "--version", action="version", version=f"%(prog)s {__version__}")
     if len(sys.argv)==1:
         par.print_help(sys.stderr)
         sys.exit(1)
     args = par.parse_args()
     pb(10)
-    html_str = clipps_str_to_html_str(read_file(args.clipps_path))
-    pb(25)    
-    html_path = args.clipps_path.replace(".txt", ".html")
+    html_str = clipps_str_to_html_str(read_file(args.kindle_clippings_path))
+    pb(25)
+    html_path = args.kindle_clippings_path.replace(".txt", ".html")
     pb(50)    
     if not args.no_style:
         html_str = style_html_str(html_str)
