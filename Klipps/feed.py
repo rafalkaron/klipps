f04b7b8c275cd7fe799fc6144ae382461905d2a2
rafalkaron <rafalkaron@gmail.com>
2020-04-30 21:58:27 +0200

    baldurs

--- Klipps/feed.py
+++ Klipps/feed.py
@@ -5,17 +5,17 @@ import sys
 __author__ = "Rafał Karoń <rafalkaron@gmail.com>"
 
 def clipps_filepath():
-    """Return the \"My Clippings.txt\" file path"""
+    """Automatically return the \"My Clippings.txt\" file path.
+    Prompt the user to provide the \"My Clippings.txt\" file path as a fallback."""
 
     if os.name == "nt":
-        volumes = ["D", "E", "F", "G", "H", "I", "J", "K", "L", "manual"]
-        for volume in volumes:
-            print(volume)
-            clipps_path = volume + r":\documents\My Clippings.txt"
+        drive_letters = ["D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "manual"]
+        for drive_letter in drive_letters:
+            clipps_path = drive_letter + r":\documents\My Clippings.txt"
             clipps_exist = os.path.isfile(clipps_path)
             if clipps_exist:
                 return clipps_path
-            if volume == "manual":
+            if drive_letter == "manual":
                 enter_clipps_filepath()
             if not clipps_exist:
                 continue
@@ -36,9 +36,9 @@ Klipps cannot locate the \"My Clippings.txt\" file that is usually in the \"docu
 Before running Klipps again, ensure that:
   * You highlighted some quotes while reading on your Kindle
   * Your Kindle is connected to and discovered by your computer
-    NOTE: Some third-party USB cables may prevent your computer from correctly discovering your Kindle
+    NOTE: Some third-party USB cables may prevent your computer from correctly discovering your Kindle.
 
-You can manually provide the \"My Clippings.txt\" file path or press [Enter] to exit Klipps: """).replace("\"", "").replace("\'","")
+You can manually enter the \"My Clippings.txt\" file path or press [Enter] to exit Klipps: """).replace("\"", "").replace("\'","")
     filepath_exist = os.path.isfile(clipps_path)
     if filepath_exist == False:
         sys.exit(0)    
--- __main__.py
+++ __main__.py
@@ -7,37 +7,34 @@ import sys
 import re
 import os
 import argparse
-from Klipps import (read_file,
+from Klipps import (clipps_filepath,
+                    read_file,
+                    progressbar as pb,
                     clipps_str_to_html_str,
                     style_html_str,
                     save_str_as_file,
                     open_file,
-                    clipps_filepath,
-                    exit_prompt,
-                    progressbar as pb)
+                    exit_prompt)
 
 __version__ = "0.7"
 __author__ = "Rafał Karoń <rafalkaron@gmail.com.com>"
 
 def main():
-    par = argparse.ArgumentParser(description="Export Kindle clippings to HTML.", formatter_class=argparse.RawTextHelpFormatter)
-    par.add_argument("-in", "--input", help="Specify the \"My Clippings.txt\" file path manually.")
-    par.add_argument("-out", "--output", help="Specify the HTML file path converted from the \"My Clippings.txt\" file manually.")
-    par.add_argument("-nopr", "--no_preview", action="store_true", help="does not open the html file converted from the \"My Clippings.txt\" file")
-    par.add_argument("-ns", "--no_style", action ="store_true", help="does not add CSS styling to the html file converted from the \"My Clippings.txt\" file")
+    par = argparse.ArgumentParser(description="Export clippings from Kindle to a HTML file on your desktop.", formatter_class=argparse.RawTextHelpFormatter)
     par.add_argument("-v", "--version", action="version", version=f"%(prog)s {__version__}")
+    par.add_argument("-in", "--input", metavar="clippings_file_path", help="manually specify the \"My Clippings.txt\" file path (defaults to the \"My Clippings.txt\" file path on a connected Kindle device)")
+    par.add_argument("-out", "--output", metavar="output_folder", help="manually specify the output folder for the HTML file with converted clippings (defaults to desktop)")
+    par.add_argument("-nopr", "--no_preview", action="store_true", help="do not automatically open the HTML file with converted clippings")
+    par.add_argument("-ns", "--no_style", action ="store_true", help="do not add CSS styling to the HTML file with converted clippings")
     args = par.parse_args()
-    
     if not args.input:
         in_path = clipps_filepath()
     if args.input:
         in_path = args.input
-
     if not args.output:
         out_path = f"{os.path.normpath(os.path.expanduser('~/Desktop'))}/{os.path.basename(in_path)}".replace(".txt", ".html").replace("\\", "/").replace("//", "/")
     if args.output:
         out_path = f"{args.output}/{os.path.basename(in_path)}".replace(".txt", ".html").replace("\\", "/").replace("//", "/")
-
     pb(10)
     html_str = clipps_str_to_html_str(read_file(in_path))
     pb(50)    
@@ -54,7 +51,6 @@ def main():
     except(PermissionError):
         pb(100)
         print(f"Klipps cannot save the converted Kindle clippings as: \"{out_path}\" because you lack permissions.\nTry running Klipps again as an administrator or selecting another directory.")
-
     exit_prompt("\nTo exit Klipps, press [Enter]")
 
 if __name__ == "__main__":
