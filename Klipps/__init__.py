500c33abc14149a1140e57c3c42af88009f9532a
rafalkaron <rafalkaron@gmail.com>
2021-02-09 19:04:53 +0100

    Deploy website - based on f471f98ba1dfc00c4bcc384ea0d9b9dc18992d97

--- Klipps/__init__.py
+++
@@ -1,7 +0,0 @@
-# coding: utf-8
-__author__ = 'Rafał Karoń <rafalkaron@gmail.com>'
-
-from .feed import get_clipps_filepath, read_file
-from .convert import clipps_str_to_html_str, default_style_html_str, custom_style_html_str
-from .publish import save_str_as_file, open_file
-from .feedback import progressbar, exit_prompt
